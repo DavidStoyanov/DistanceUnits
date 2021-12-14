7d7514cd5c596285946b6ae0fb974aadbcdbb7d4
DavidStoyanov <david_st1@abv.bg>
2021-12-14 23:10:24 +0200

    New Metrics added - Feet - Kilometer

---
+++ DistanceUnits/Metrics/Feet.cs
@@ -0,0 +1,16 @@
+﻿using DistanceUnits.Constants;
+
+namespace DistanceUnits.metrics
+{
+    public class Feet : Metric
+    {
+        public Feet(decimal length) : base(length)
+        {
+        }
+
+        public override decimal lengthToMeters()
+        {
+            return Length * Units.FeetToMeters;
+        }
+    }
+}
---
+++ DistanceUnits/Metrics/Kilometer.cs
@@ -0,0 +1,16 @@
+﻿using DistanceUnits.Constants;
+
+namespace DistanceUnits.metrics
+{
+    public class Kilometer : Metric
+    {
+        public Kilometer(decimal length) : base(length)
+        {
+        }
+
+        public override decimal lengthToMeters()
+        {
+            return Length * Units.KilometersToMeters;
+        }
+    }
+}
