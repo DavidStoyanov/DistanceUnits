e33c11a42fb32c6a1f3a07b3486864cb2bfcd9fa
DavidStoyanov <david_st1@abv.bg>
2021-12-15 01:31:51 +0200

    Remove unnecessary code from Main

--- DistanceMain/Program.cs
+++ DistanceMain/Program.cs
@@ -10,11 +10,7 @@ namespace DistanceMain
     {
         public static void Main(string[] args)
         {
-            Distance<IMetrics> a = Distance<IMetrics>.FromMeters(150);
-            Distance<IMetrics> b = Distance<IMetrics>.FromMeters(70);
-            Console.WriteLine((a + b).AsMeters());
             
-            //todo: Add KM FEET
         }
     }
 }
